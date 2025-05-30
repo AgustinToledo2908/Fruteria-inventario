3ad7795f21cf3cec189a32047f12d4c5b220e92a
agustintoledo2908 <agustintoledo2908@gmail.com>
2025-05-29 22:05:55 -0300

    se creo la ruta del post y se probo correctamente

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const { addProduct } = require("../controllers/inventoryController");
+
 router.get("/", (req, res) => {
   res.json({ message: "Inventario funcionando" });
 });
+
+router.post("/", addProduct);
 
 module.exports = router;
